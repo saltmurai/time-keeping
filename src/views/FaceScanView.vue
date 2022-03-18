210a07d0d6abcc69cbe14da6d54ff4a5830b9aef
flyg0n <phamsang6260@gmail.com>
2022-03-18 14:03:53 +0700

    Camera working

--- src/views/FaceScanView.vue
+++ src/views/FaceScanView.vue
@@ -1,34 +1,71 @@
 <template>
-  <div class="teal fill-height">
-    <v-row justify="center">
-      <v-dialog v-model="dialog" persistent max-width="290">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn color="primary" dark v-bind="attrs" v-on="on">
-            Open Dialog
-          </v-btn>
-        </template>
-        <ErrorCard :error="error"></ErrorCard>
-      </v-dialog>
-    </v-row>
+  <div class="container">
+    <video class="input_video" width="100%" height="100%"></video>
+    <div class="canvas-container">
+      <canvas class="output_canvas" width="1280px" height="720px"> </canvas>
+    </div>
   </div>
 </template>
-
 <script>
-import ErrorCard from "@/components/ErrorCard";
+import { FaceDetection } from "@mediapipe/face_detection";
+import { Camera } from "@mediapipe/camera_utils";
+import { drawRectangle, drawLandmarks } from "@mediapipe/drawing_utils";
+
 export default {
-  components: {
-    ErrorCard,
+  name: "FaceModel",
+  data: function () {
+    return {};
   },
-  data() {
-    return {
-      dialog: true,
-      error: {
-        icon: "mdi-domain",
-        mess: "Error",
-        detail: "Details of Error",
-        buttonText: "Thu lai",
-      },
-    };
+  mounted() {
+    this.init();
+  },
+  methods: {
+    onResults(results) {
+      const canvasElement = document.getElementsByClassName("output_canvas")[0];
+      const ctx = canvasElement.getContext("2d");
+      ctx.save();
+      ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+      ctx.drawImage(
+        results.image,
+        0,
+        0,
+        canvasElement.width,
+        canvasElement.height
+      );
+      if (results.detections.length > 0) {
+        drawRectangle(this.ctx, results.detections[0].boundingBox, {
+          color: "blue",
+          lineWidth: 4,
+          fillColor: "#00000000",
+        });
+        drawLandmarks(this.ctx, results.detections[0].landmarks, {
+          color: "red",
+          radius: 5,
+        });
+      }
+      this.ctx.restore();
+    },
+    init() {
+      const inputVideo = document.getElementsByClassName("input_video")[0];
+      const face = new FaceDetection({
+        locateFile: (file) => {
+          return `https://cdn.jsdelivr.net/npm/@mediapipe/face_detection@0.4/${file}`;
+        },
+      });
+      face.setOptions({
+        modelSelection: "short",
+        minDetectionConfidence: 0.5,
+      });
+      face.onResults(this.onResults);
+
+      const camera = new Camera(inputVideo, {
+        onFrame: async () => {
+          await face.send({ image: inputVideo });
+        },
+      });
+      camera.start();
+    },
   },
 };
 </script>
+<style scoped></style>
